3e9f26d482bdc609942f9d43d9aabdcc33db6de8
Arnold Dajao <arnold.dajao@ironnetcybersecurity.com>
2021-08-16 15:48:31 -0400

    updated version

--- Jenkinsfile
+++ Jenkinsfile
@@ -24,6 +24,7 @@ pipeline {
         }
      }
     stages {
+
         stage("test") {
 //           withEnv(["HOME=${env.WORKSPACE}"]) {
             steps {
--- scripts/automation.py
+++ scripts/automation.py
@@ -1,1 +1,70 @@
+import self as self
+
 print("test from echo file")
+
+import os
+import git
+
+# def gitDiff(branch1, branch2):
+#     format = '--name-only'
+#     commits = []
+#     g = git.Git('https://github.com/arnoldmd181/piSandBox')
+#     differ = g.diff('%s..%s' % (branch1, branch2), format).split("\n")
+#     for line in differ:
+#         if len(line):
+#             commits.append(line)
+#
+#     #for commit in commits:
+#     #    print '*%s' % (commit)
+#     return commits
+#
+# # repo = versions("/Users/arnold.dajao/Documents/OldTask/Temp/Iron-predict-models-test")
+# repo = gitDiff("test", "dev")
+#
+# print(repo)
+
+# repo = git.Repo("/Users/arnold.dajao/Documents/OldTask/Temp/Iron-predict-models-test")
+# # logger.info("repo": repo)
+# print("repo" + repo)
+# subprocess.check_output(['git', 'diff', '--name-only', currentBranch + '..' + compBranch])
+
+# branch1  = "dev"
+# branch2 = "test1"
+# g = git.Git('https://github.com/arnoldmd181/piSandBox')
+#
+# print(g)
+#
+# format = '--name-only'
+# commits = []
+# g = git.Git('https://github.com/arnoldmd181/piSandBox')
+# # differ = g.diff('%s..%s' % (branch1, branch2), format).split("\n")
+# different = g.diff("git diff dev…test1")
+# print(different)
+# # for line in differ:
+# #     if len(line):
+# #         print(line)
+
+from git import Repo
+from pathlib import Path
+
+# rorepo is a Repo instance pointing to the git-python repository.
+# For all you know, the first argument to Repo is a path to the repository
+# you want to work with
+repo = Repo("/Users/arnold.dajao/Documents/OldTask/Temp/piSandBox")
+
+commit_dev = repo.commit("test1")
+commit_origin_dev = repo.commit("origin/dev")
+diff_index = commit_origin_dev.diff(commit_dev)
+
+classifiers_updates = set()
+classifiers = {"phishing_common", "domain_classifier", "filename_classifier", "path_classifier",
+               "tls_certificate_classifier"}
+
+for diff_item in diff_index:
+    path = Path(diff_item.a_path).parent
+    print("A blob: {} \n".format(path))
+    if str(path).lower() in classifiers:
+        classifiers_updates.add(path)
+
+
+print(classifiers_updates)
