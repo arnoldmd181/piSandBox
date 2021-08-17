731f5c7822b5ef10482040b9114936c66ac9ebc2
Arnold Dajao <arnold.dajao@ironnetcybersecurity.com>
2021-08-16 22:00:37 -0400

    logic testing

---
+++ automation.py
@@ -0,0 +1,68 @@
+import os
+
+import switch as switch
+from git import Repo
+from pathlib import Path
+import re
+
+PWD = os.path.abspath(os.path.dirname(__file__))
+MAIN_REPO = "origin/dev"
+CLASSIFIERS = {"phishing_common", "domain_classifier", "filename_classifier", "path_classifier",
+               "tls_certificate_classifier"}  # TODO does need to be in config file?
+
+
+def increment_version(version):
+    pattern = re.compile("""(\\d\\.\\d\\.)(\\d)(.*)""")
+    matches = pattern.match(version)
+    return matches.group(1) + str(int(matches.group(2)) + 1) + matches.group(3)
+
+
+# update dependency version or update _version.py
+def update_version_deps(file_name, version, new_version):
+    with open(file_name) as f:
+        new_text = f.read().replace(version, new_version)
+    with open(file_name, "w") as f:
+        f.write(file_name)
+
+# pattern matching similar to scala is only available in 3.10
+def update_version_classifiers(file_name, version, new_version, update_classifier):
+
+    update_version_deps(file_name, version, new_version)
+
+    if update_classifier == "phishing_common":
+        for classifier_setup in CLASSIFIERS.pop():
+            setup_file = PWD + f'/{classifier_setup}/setup.py'
+            update_version_deps(setup_file, version, new_version)
+    elif update_classifier == "domain_classifier":
+        setup_file = PWD + f'/tls_certificate_classifier/setup.py'
+        update_version_deps(setup_file, version, new_version)
+
+
+
+repo = Repo("/Users/arnold.dajao/Documents/OldTask/Temp/piSandBox")  # TODO get from variables
+
+commit_dev = repo.commit("test1")  # TODO get from variables
+commit_origin_dev = repo.commit(MAIN_REPO)
+diff_index = commit_origin_dev.diff(commit_dev)
+
+classifiers_updates = set()
+
+for diff_item in diff_index:
+    path = Path(diff_item.a_path).parent
+    if str(path).lower() in CLASSIFIERS:
+        classifiers_updates.add(path)
+
+
+
+for classifier in classifiers_updates
+    print(classifier)
+
+    version_file = PWD + f'/{classifier}/{classifier}/_version.py'
+    # Read Version file
+    exec(open(version_file).read())
+
+    classifiers_version = __version__
+    # update version
+    new_version_incr = increment_version(version_file)
+
+    update_version_classifiers(version_file, classifiers_version, new_version_incr, classifier)
---
+++ path_classifier/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+exec(open(os.path.join(here, 'domain_classifier/_version.py')).read())
+
+setup(
+    name='domain_classifier',
+    version=__version__,
+    packages=find_packages(),
+    description='Domain threat prediction model for phishing',
+    url='https://github.com/IronNetCybersecurity/iron-predict-models',
+    install_requires=[
+        'cloudpickle',
+        'h5py==2.9.0',
+        'Keras==2.2.4',
+        'numpy==1.18.2',
+        'pandas',
+        'progressbar',
+        'pyahocorasick',
+        'scikit-learn==0.20.3',
+        'scipy==1.4.1',
+        'tensorflow==1.15.0',
+        'tldextract',
+        'urllib3',
+        # Internal ironnet projects
+        'phishing_common==0.1.0_dev2'
+    ],
+    include_package_data=False
+)
